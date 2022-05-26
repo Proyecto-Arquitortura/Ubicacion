3b2d6ce8caeded0ed4c58e92bdd14c68f4113dc7
Oscar Pacheco <oscarpa_2000@hotmail.com>
2022-05-26 14:31:01 -0500

    added orm

--- smart_uj/__init__.py
+++ smart_uj/__init__.py
@@ -1,10 +1,7 @@
-import logging
 from typing import Any
 
 from flask import Flask
-
 from flask_sqlalchemy import SQLAlchemy
-
 from flask_cors import CORS
 
 
@@ -17,13 +14,6 @@ def init_app(config_file_path: str = "settings.py", **config: Any) -> Flask:
     app = Flask(__name__, instance_relative_config=False)
     app.config.from_pyfile(config_file_path)
     app.config.update(**config)
-
-    """Logging configuration"""
-    logging.basicConfig(filename='./logs/logs.log', level=logging.DEBUG,
-                        format=f'%(asctime)s %(levelname)s %(name)s %(threadName)s : %(message)s')
-    app.logger.info('Info level log')
-    app.logger.warning('Warning level log')
-    app.logger.error('Error level log')
 
     """ Initialize Plugins """
     db.init_app(app)
